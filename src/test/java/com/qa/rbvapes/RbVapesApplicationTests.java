cd48cfab735ee07d614cb157a8eb3c01f391a5a0
Alex <abaron756@gmail.com>
2022-03-06 15:29:25 +0000

    Initial Commit

---
+++ src/test/java/com/qa/rbvapes/RbVapesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.rbvapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbVapesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
