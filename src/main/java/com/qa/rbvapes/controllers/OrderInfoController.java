2d264bf835082a0cdc1a5cb021e5e86fa2c9ece0
Alex <abaron756@gmail.com>
2022-03-07 15:03:07 +0000

    Create Order Info

--- src/main/java/com/qa/rbvapes/controllers/OrderInfoController.java
+++ src/main/java/com/qa/rbvapes/controllers/OrderInfoController.java
@@ -1,11 +1,25 @@
 package com.qa.rbvapes.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.rbvapes.domains.OrderInfo;
 import com.qa.rbvapes.services.OrderInfoService;
 
 @RestController
 public class OrderInfoController {
 
 	private OrderInfoService service;
+
+	public OrderInfoController(OrderInfoService service) {
+		super();
+		this.service = service;
+	}
+
+	@PostMapping("/createOrderInfo")
+	public OrderInfo createInfo(@RequestBody OrderInfo info) {
+		return this.service.create(info);
+	}
+
 }
--- src/main/java/com/qa/rbvapes/services/OrderInfoService.java
+++ src/main/java/com/qa/rbvapes/services/OrderInfoService.java
@@ -2,10 +2,21 @@ package com.qa.rbvapes.services;
 
 import org.springframework.stereotype.Service;
 
+import com.qa.rbvapes.domains.OrderInfo;
 import com.qa.rbvapes.repos.OrderInfoRepo;
 
 @Service
 public class OrderInfoService {
 
 	private OrderInfoRepo repo;
+
+	public OrderInfoService(OrderInfoRepo repo) {
+		super();
+		this.repo = repo;
+	}
+
+	public OrderInfo create(OrderInfo info) {
+		return this.repo.save(info);
+	}
+
 }
