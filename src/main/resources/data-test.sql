d1ec77ee47c2a8e96bbd43e1f565ca46cc7494ce
Alex <abaron756@gmail.com>
2022-03-10 11:47:40 +0000

    Flavour Controller tested and Reaponse Entites added

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -5,3 +5,7 @@ VALUES
 INSERT INTO brands (`BRAND_NAME`, `PUFF_COUNT`, `PRICE`)
 VALUES
 ('AromaKing', 7000, 15.00);
+
+INSERT INTO flavours (`FLAVOUR_NAME`, `BRAND_NAME`, `QUANTITY`)
+VALUES
+('CottonCandy', 'AromaKing', 15);
