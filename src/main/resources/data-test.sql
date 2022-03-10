3aa1f842b90bf8c9fdb334c1db2c8ce1205bea24
Alex <abaron756@gmail.com>
2022-03-10 11:23:37 +0000

    Brand Controller tested and Response Entites added

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -1,3 +1,7 @@
 INSERT INTO customers (`NAME`, `ADDRESS`, `PHONE_NUMBER`)
 VALUES
 ('Alex', 'Address', 'Number');
+
+INSERT INTO brands (`BRAND_NAME`, `PUFF_COUNT`, `PRICE`)
+VALUES
+('AromaKing', 7000, 15.00);
